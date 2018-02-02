e6bdadae5e0fe5b418d604b0195169dd63e54ca9
trishamf22 <35825603+trishamf22@users.noreply.github.com>
2018-02-01 17:13:37 -0800

    Add files via upload

---
+++ src/assignments/main_assignment2.py
@@ -0,0 +1,5 @@
+from assignment2 import faculty_evaluation_result
+
+'''Write code to call the faculty_evaluation_result function with data of your choice'''
+result = faculty_evaluation_result(20, 30, 78, 26, 76, 92)
+print(result)
